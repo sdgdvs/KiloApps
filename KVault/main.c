5b4c67ef1911818e45069b1afd877ab8b7b5fc5c
anonymous2 <sdgdvs@protonmail.com>
2026-07-11 18:02:06 -0700

    Polish KVault UI for Phase 4

--- KVault/main.c
+++ KVault/main.c
@@ -11,6 +11,8 @@
 
 HWND hPass, hData;
 HBRUSH hbgBrush;
+HBRUSH hDarkBrush;
+HFONT hFont, hTitleFont;
 
 void EncryptData(HWND hTextEdit, HWND hPassEdit) {
     int textLen = GetWindowTextLengthA(hTextEdit);
@@ -62,24 +64,29 @@ void DecryptData(HWND hTextEdit, HWND hPassEdit) {
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch(msg) {
         case WM_CREATE: {
-            hbgBrush = CreateSolidBrush(RGB(30, 30, 30));
-            HFONT hFont = CreateFont(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Segoe UI");
+            hbgBrush = CreateSolidBrush(RGB(15, 15, 19)); // Darker background
+            hDarkBrush = CreateSolidBrush(RGB(25, 25, 30));
+            hFont = CreateFont(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Segoe UI");
+            hTitleFont = CreateFont(24, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Segoe UI");
             
-            HWND hStat = CreateWindowA("STATIC", "Password:", WS_VISIBLE | WS_CHILD, 10, 12, 70, 25, hwnd, NULL, NULL, NULL);
+            HWND hTitle = CreateWindowA("STATIC", "KVault - Secure Storage", WS_VISIBLE | WS_CHILD, 15, 15, 300, 30, hwnd, NULL, NULL, NULL);
+            SendMessage(hTitle, WM_SETFONT, (WPARAM)hTitleFont, TRUE);
+            
+            HWND hStat = CreateWindowA("STATIC", "Master Key:", WS_VISIBLE | WS_CHILD, 15, 62, 80, 25, hwnd, NULL, NULL, NULL);
             SendMessage(hStat, WM_SETFONT, (WPARAM)hFont, TRUE);
             
-            hPass = CreateWindowA("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_PASSWORD | ES_AUTOHSCROLL, 85, 10, 215, 25, hwnd, (HMENU)ID_EDIT_PASS, NULL, NULL);
+            hPass = CreateWindowA("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_PASSWORD | ES_AUTOHSCROLL, 100, 60, 200, 25, hwnd, (HMENU)ID_EDIT_PASS, NULL, NULL);
             SendMessage(hPass, WM_SETFONT, (WPARAM)hFont, TRUE);
             
-            HWND hBtnEnc = CreateWindowA("BUTTON", "Encrypt", WS_VISIBLE | WS_CHILD, 310, 10, 80, 25, hwnd, (HMENU)ID_BTN_ENCRYPT, NULL, NULL);
-            HWND hBtnDec = CreateWindowA("BUTTON", "Decrypt", WS_VISIBLE | WS_CHILD, 400, 10, 80, 25, hwnd, (HMENU)ID_BTN_DECRYPT, NULL, NULL);
-            HWND hBtnClr = CreateWindowA("BUTTON", "Clear", WS_VISIBLE | WS_CHILD, 490, 10, 80, 25, hwnd, (HMENU)ID_BTN_CLEAR, NULL, NULL);
+            HWND hBtnEnc = CreateWindowA("BUTTON", "Encrypt", WS_VISIBLE | WS_CHILD | BS_FLAT, 315, 60, 85, 25, hwnd, (HMENU)ID_BTN_ENCRYPT, NULL, NULL);
+            HWND hBtnDec = CreateWindowA("BUTTON", "Decrypt", WS_VISIBLE | WS_CHILD | BS_FLAT, 410, 60, 85, 25, hwnd, (HMENU)ID_BTN_DECRYPT, NULL, NULL);
+            HWND hBtnClr = CreateWindowA("BUTTON", "Clear", WS_VISIBLE | WS_CHILD | BS_FLAT, 505, 60, 75, 25, hwnd, (HMENU)ID_BTN_CLEAR, NULL, NULL);
             
             SendMessage(hBtnEnc, WM_SETFONT, (WPARAM)hFont, TRUE);
             SendMessage(hBtnDec, WM_SETFONT, (WPARAM)hFont, TRUE);
             SendMessage(hBtnClr, WM_SETFONT, (WPARAM)hFont, TRUE);
             
-            hData = CreateWindowA("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_WANTRETURN, 10, 45, 560, 305, hwnd, (HMENU)ID_EDIT_DATA, NULL, NULL);
+            hData = CreateWindowA("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_WANTRETURN, 15, 100, 565, 250, hwnd, (HMENU)ID_EDIT_DATA, NULL, NULL);
             SendMessage(hData, WM_SETFONT, (WPARAM)hFont, TRUE);
             
             break;
@@ -96,20 +103,21 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
         }
         case WM_CTLCOLORSTATIC: {
             HDC hdc = (HDC)wParam;
-            SetTextColor(hdc, RGB(220, 220, 220));
-            SetBkColor(hdc, RGB(30, 30, 30));
+            SetTextColor(hdc, RGB(240, 240, 245));
+            SetBkColor(hdc, RGB(15, 15, 19));
             return (LRESULT)hbgBrush;
         }
         case WM_CTLCOLOREDIT: {
             HDC hdc = (HDC)wParam;
-            SetTextColor(hdc, RGB(220, 220, 220));
-            SetBkColor(hdc, RGB(45, 45, 45));
-            static HBRUSH hEditBrush = NULL;
-            if (!hEditBrush) hEditBrush = CreateSolidBrush(RGB(45, 45, 45));
-            return (LRESULT)hEditBrush;
+            SetTextColor(hdc, RGB(240, 240, 245));
+            SetBkColor(hdc, RGB(25, 25, 30));
+            return (LRESULT)hDarkBrush;
         }
         case WM_DESTROY: {
             DeleteObject(hbgBrush);
+            DeleteObject(hDarkBrush);
+            DeleteObject(hFont);
+            DeleteObject(hTitleFont);
             PostQuitMessage(0);
             break;
         }
@@ -124,7 +132,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = "KVaultClass";
-    wc.hbrBackground = CreateSolidBrush(RGB(30, 30, 30));
+    wc.hbrBackground = CreateSolidBrush(RGB(15, 15, 19));
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     
     RegisterClassA(&wc);
