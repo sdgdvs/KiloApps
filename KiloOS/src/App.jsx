9379a5446d7df5d57bf180d6d8aea1b547e8ba29
sdgdvs <sdgdvs@protonmail.com>
2026-07-12 06:32:03 -0700

    UX: Improved desktop icon styling and theme consistency

--- KiloOS/src/App.jsx
+++ KiloOS/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect, useCallback } from 'react';
 import { DEFAULT_VFS } from './defaultVfs';
 import './App.css';
-const MICROS_VERSION = '0.3.46';
+const MICROS_VERSION = '0.3.47';
 
 const FOLDER_ICON = "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23ffd700'><path d='M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z'/></svg>";
 
@@ -315,7 +315,7 @@ function Window({ app, onClose, onFocus, onMinimize, vfs, setVfs, requestVfsModa
                   onClick={(e) => { e.stopPropagation(); }}
                   onDoubleClick={(e) => { e.stopPropagation(); window.dispatchEvent(new CustomEvent('os-launch-app', { detail: { appId: child.id, path: '' } })); }}
                 >
-                  <img src={child.icon} alt={child.title} style={{width:'32px', height:'32px', imageRendering: 'pixelated', marginTop: '4px'}} />
+                  <img src={child.icon} alt={child.title} />
                   <div className="icon-label">{child.title}</div>
                 </div>
               ))
@@ -862,7 +862,7 @@ function App() {
             onDoubleClick={(e) => { e.stopPropagation(); playClickAudio(); setStartOpen(false); setContextMenu(null); openApp({ ...folder, isFolder: true }); }}
             onContextMenu={(e) => { e.stopPropagation(); playClickAudio(); e.preventDefault(); setSelectedIcons([folder.id]); setContextMenu({ type: 'folder', id: folder.id, x: e.clientX, y: e.clientY }); }}
           >
-            <img src={folder.icon} alt={folder.title} style={{width:'32px', height:'32px', imageRendering: 'pixelated'}} />
+            <img src={folder.icon} alt={folder.title} />
             <div className="icon-label">{folder.title}</div>
           </div>
         ))}
@@ -876,7 +876,7 @@ function App() {
             onDoubleClick={(e) => { e.stopPropagation(); playClickAudio(); setStartOpen(false); setContextMenu(null); openApp(app); }}
             onContextMenu={(e) => { e.stopPropagation(); playClickAudio(); e.preventDefault(); setSelectedIcons([app.id]); setContextMenu({ type: 'app', id: app.id, x: e.clientX, y: e.clientY }); }}
           >
-            <img src={app.icon} alt={app.title} style={{width:'32px', height:'32px', imageRendering: 'pixelated'}} />
+            <img src={app.icon} alt={app.title} />
             <div className="icon-label">{app.title}</div>
           </div>
         ))}
