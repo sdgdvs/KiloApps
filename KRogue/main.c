415feb3b4ec45dc2ce3d19bf1b10c3b2d10a1d3d
sdgdvs <sdgdvs@protonmail.com>
2026-07-17 10:33:38 -0700

    Games: expand KRogue with 15 levels, Ghost/Hydra/Cube, Lightning spell, Biomes, Stats

--- KRogue/main.c
+++ KRogue/main.c
@@ -28,6 +28,9 @@
 #define C_SHOPKEEPER RGB(255,215,0)
 #define C_SKELETON RGB(220, 220, 220)
 #define C_DEMON RGB(200, 0, 50)
+#define C_GHOST RGB(150, 200, 255)
+#define C_HYDRA RGB(50, 200, 100)
+#define C_CUBE  RGB(0, 255, 255)
 
 #define C_POTION RGB(255,50,255)
 #define C_WEAPON RGB(50,255,255)
@@ -130,6 +133,7 @@ typedef struct {
 #define S_HEAL 1
 #define S_FIREBALL 2
 #define S_FREEZE 3
+#define S_LIGHTNING 4
 
 typedef struct {
     Tile map[H][W];
@@ -153,6 +157,7 @@ typedef struct {
     int active_spell;
     Entity* active_shopkeeper;
     int difficulty; // 0=Normal, 1=Hard, 2=Nightmare
+    int total_kills;
 } GameState;
 
 GameState g;
@@ -263,7 +268,7 @@ void spawn_monster(int x, int y) {
             }
             
             int tier = rand_range(1, max_tier);
-            int m = rand_range(0, 4);
+            int m = rand_range(0, 5);
             
             if(tier == 1) {
                 if(m == 0) { e->ch = 'r'; e->fg = C_RAT; str_cpy(e->name, "Giant Rat"); e->hp = e->max_hp = 5 + g.dlevel; e->atk = 2; e->def = 1; e->xp = 2; e->behavior = B_NORMAL; }
@@ -276,19 +281,22 @@ void spawn_monster(int x, int y) {
                 else if(m == 1) { e->ch = 'S'; e->fg = C_SPIDER; str_cpy(e->name, "Giant Spider"); e->hp = e->max_hp = 10 + g.dlevel*2; e->atk = 4; e->def = 1; e->xp = 6; e->behavior = B_FAST; }
                 else if(m == 2) { e->ch = 'z'; e->fg = C_ZOMBIE; str_cpy(e->name, "Zombie"); e->hp = e->max_hp = 25 + g.dlevel*2; e->atk = 5; e->def = 1; e->xp = 8; e->behavior = B_SLUGGISH; }
                 else if(m == 3) { e->ch = 'k'; e->fg = C_KOBOLD; str_cpy(e->name, "Kobold"); e->hp = e->max_hp = 8 + g.dlevel*2; e->atk = 3; e->def = 1; e->xp = 4; e->behavior = B_COWARD; }
-                else { e->ch = 'c'; e->fg = C_GOBLIN; str_cpy(e->name, "Centipede"); e->hp = e->max_hp = 12 + g.dlevel*2; e->atk = 4; e->def = 2; e->xp = 5; e->behavior = B_ERRATIC; }
+                else if(m == 4) { e->ch = 'c'; e->fg = C_GOBLIN; str_cpy(e->name, "Centipede"); e->hp = e->max_hp = 12 + g.dlevel*2; e->atk = 4; e->def = 2; e->xp = 5; e->behavior = B_ERRATIC; }
+                else { e->ch = 'G'; e->fg = C_GHOST; str_cpy(e->name, "Ghost"); e->hp = e->max_hp = 15 + g.dlevel*2; e->atk = 6; e->def = 2; e->xp = 10; e->behavior = B_ERRATIC; }
             } else if(tier == 3) {
                 if(m == 0) { e->ch = 'T'; e->fg = C_TROLL; str_cpy(e->name, "Cave Troll"); e->hp = e->max_hp = 30 + g.dlevel*3; e->atk = 8; e->def = 4; e->xp = 15; e->behavior = B_NORMAL; }
                 else if(m == 1) { e->ch = 'M'; e->fg = C_MINOTAUR; str_cpy(e->name, "Minotaur"); e->hp = e->max_hp = 25 + g.dlevel*3; e->atk = 10; e->def = 3; e->xp = 20; e->behavior = B_FAST; }
                 else if(m == 2) { e->ch = 'W'; e->fg = C_WRAITH; str_cpy(e->name, "Wraith"); e->hp = e->max_hp = 20 + g.dlevel*3; e->atk = 9; e->def = 5; e->xp = 18; e->behavior = B_ERRATIC; }
                 else if(m == 3) { e->ch = 'e'; e->fg = C_ELF; str_cpy(e->name, "Dark Elf"); e->hp = e->max_hp = 20 + g.dlevel*3; e->atk = 12; e->def = 3; e->xp = 16; e->behavior = B_SMART; }
-                else { e->ch = 'y'; e->fg = C_SKELETON; str_cpy(e->name, "Gargoyle"); e->hp = e->max_hp = 35 + g.dlevel*3; e->atk = 7; e->def = 8; e->xp = 16; e->behavior = B_SLUGGISH; }
+                else if(m == 4) { e->ch = 'y'; e->fg = C_SKELETON; str_cpy(e->name, "Gargoyle"); e->hp = e->max_hp = 35 + g.dlevel*3; e->atk = 7; e->def = 8; e->xp = 16; e->behavior = B_SLUGGISH; }
+                else { e->ch = 'C'; e->fg = C_CUBE; str_cpy(e->name, "Gelatinous Cube"); e->hp = e->max_hp = 50 + g.dlevel*3; e->atk = 5; e->def = 10; e->xp = 25; e->behavior = B_SLUGGISH; e->special_ability = ABILITY_SPLIT; }
             } else {
                 if(m == 0) { e->ch = 'D'; e->fg = C_DRAGON; str_cpy(e->name, "Red Dragon"); e->hp = e->max_hp = 60 + g.dlevel*5; e->atk = 15; e->def = 8; e->xp = 50; e->behavior = B_NORMAL; e->special_ability = ABILITY_BREATHE_FIRE; }
                 else if(m == 1) { e->ch = 'V'; e->fg = C_VAMPIRE; str_cpy(e->name, "Vampire"); e->hp = e->max_hp = 45 + g.dlevel*5; e->atk = 14; e->def = 6; e->xp = 45; e->behavior = B_FAST; }
                 else if(m == 2) { e->ch = 'G'; e->fg = C_GOLEM; str_cpy(e->name, "Stone Golem"); e->hp = e->max_hp = 80 + g.dlevel*5; e->atk = 10; e->def = 15; e->xp = 60; e->behavior = B_SLUGGISH; }
                 else if(m == 3) { e->ch = 'L'; e->fg = C_LICH; str_cpy(e->name, "Lich"); e->hp = e->max_hp = 40 + g.dlevel*5; e->atk = 20; e->def = 5; e->xp = 80; e->behavior = B_SMART; e->special_ability = ABILITY_SUMMON; }
-                else { e->ch = 'd'; e->fg = C_DEMON; str_cpy(e->name, "Demon"); e->hp = e->max_hp = 50 + g.dlevel*5; e->atk = 18; e->def = 10; e->xp = 70; e->behavior = B_FAST; e->special_ability = ABILITY_BREATHE_FIRE; }
+                else if(m == 4) { e->ch = 'd'; e->fg = C_DEMON; str_cpy(e->name, "Demon"); e->hp = e->max_hp = 50 + g.dlevel*5; e->atk = 18; e->def = 10; e->xp = 70; e->behavior = B_FAST; e->special_ability = ABILITY_BREATHE_FIRE; }
+                else { e->ch = 'H'; e->fg = C_HYDRA; str_cpy(e->name, "Hydra"); e->hp = e->max_hp = 90 + g.dlevel*5; e->atk = 25; e->def = 8; e->xp = 100; e->behavior = B_NORMAL; }
             }
             
             if(e->behavior != B_SHOPKEEPER) {
@@ -341,11 +349,12 @@ void generate_random_item(Item* it) {
         else { str_cpy(it->name, "Amulet of Life"); it->val = 20 + g.dlevel * 5; it->ch = '"'; }
     } else {
         it->ch = '?'; it->fg = RGB(200, 100, 255); it->type = TYPE_SPELLBOOK;
-        it->subtype = rand_range(0, 3);
+        it->subtype = rand_range(0, 4);
         if(it->subtype == S_MISSILE) { str_cpy(it->name, "Spellbook of Magic Missile"); }
         else if(it->subtype == S_HEAL) { str_cpy(it->name, "Spellbook of Healing"); }
         else if(it->subtype == S_FIREBALL) { str_cpy(it->name, "Spellbook of Fireball"); }
-        else { str_cpy(it->name, "Spellbook of Ice Storm"); }
+        else if(it->subtype == S_FREEZE) { str_cpy(it->name, "Spellbook of Ice Storm"); }
+        else { str_cpy(it->name, "Spellbook of Lightning"); }
     }
 }
 
@@ -454,7 +463,7 @@ void generate_map() {
     get_player()->x = cx;
     get_player()->y = cy;
     
-    if(g.dlevel == 10) {
+    if(g.dlevel == 15) {
         g.stair_x = -1;
         g.stair_y = -1;
         for(int i=1; i<MAX_ENTITIES; i++) {
@@ -517,8 +526,21 @@ void generate_map() {
         }
     }
     
+
+    COLORREF cur_wall = C_WALL;
+    COLORREF cur_floor = C_FLOOR;
+    if(g.dlevel >= 5 && g.dlevel < 10) { cur_wall = RGB(100, 150, 100); cur_floor = RGB(50, 80, 50); }
+    else if(g.dlevel >= 10 && g.dlevel < 15) { cur_wall = RGB(150, 50, 50); cur_floor = RGB(80, 40, 40); }
+    else if(g.dlevel >= 15) { cur_wall = RGB(100, 0, 100); cur_floor = RGB(50, 0, 50); }
+    for(int y=0; y<H; y++) {
+        for(int x=0; x<W; x++) {
+            if(g.map[y][x].ch == '#') g.map[y][x].fg = cur_wall;
+            if(g.map[y][x].ch == '.') g.map[y][x].fg = cur_floor;
+        }
+    }
     calc_fov_bresenham();
 }
+
 
 void init_game() {
     RtlZeroMemory(&g, sizeof(GameState));
@@ -644,6 +666,7 @@ void handle_death(Entity* e, Entity* killer) {
     } else {
         if(killer == get_player()) {
             gain_xp(killer, e->xp);
+            g.total_kills++;
         } else if(killer != NULL) {
             killer->xp += e->xp;
         }
@@ -1297,6 +1320,7 @@ void draw_game(HDC hdc) {
         wsprintfA(buf, "Level: %d", p->level); TextOutA(memDC, 20, 140, buf, str_len(buf));
         wsprintfA(buf, "XP:    %d / %d", p->xp, p->level * 20); TextOutA(memDC, 20, 160, buf, str_len(buf));
         wsprintfA(buf, "Food:  %d / 1000", p->hunger); TextOutA(memDC, 20, 180, buf, str_len(buf));
+        wsprintfA(buf, "Kills: %d", g.total_kills); TextOutA(memDC, 200, 180, buf, str_len(buf));
         
         wsprintfA(buf, "STR: %d", p->str); TextOutA(memDC, 20, 200, buf, str_len(buf));
         wsprintfA(buf, "DEX: %d", p->dex); TextOutA(memDC, 20, 220, buf, str_len(buf));
@@ -1329,6 +1353,10 @@ void draw_game(HDC hdc) {
         }
         if(g.known_spells[S_FREEZE]) {
             TextOutA(memDC, 20, y, "d - Ice Storm (Cost: 6 MP)", 26);
+            y += char_h;
+        }
+        if(g.known_spells[S_LIGHTNING]) {
+            TextOutA(memDC, 20, y, "e - Lightning (Cost: 7 MP)", 26);
             y += char_h;
         }
     } else if(g.state == 8) { // message log
@@ -1528,6 +1556,39 @@ void fire_spell() {
                         handle_death(target, p);
                     }
                 }
+            }
+        }
+    } else if (g.active_spell == S_LIGHTNING) {
+        int x0 = p->x, y0 = p->y;
+        int x1 = g.target_x, y1 = g.target_y;
+        int dx = (x1 > x0 ? x1 - x0 : x0 - x1);
+        int dy = -(y1 > y0 ? y1 - y0 : y0 - y1);
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx + dy, e2;
+        
+        p->mp -= 7;
+        add_msg("You cast Lightning!");
+        
+        while(1) {
+            if(x0 == x1 && y0 == y1) break;
+            e2 = 2 * err;
+            if(e2 >= dy) { err += dy; x0 += sx; }
+            if(e2 <= dx) { err += dx; y0 += sy; }
+            
+            Entity* target = get_entity_at(x0, y0);
+            if(target && target != p) {
+                char buf[100];
+                int dmg = 15 + p->level * 3;
+                target->hp -= dmg;
+                wsprintfA(buf, "Lightning zaps %s for %d dmg.", target->name, dmg);
+                add_msg(buf);
+                if(target->hp <= 0) {
+                    handle_death(target, p);
+                }
+            }
+            if(!g.map[y0][x0].walkable && !g.map[y0][x0].transparent) {
+                break; // stop at wall
             }
         }
     }
