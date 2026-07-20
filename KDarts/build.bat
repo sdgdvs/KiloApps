ddfeaada1f329f7468add1d102a4400affddce00
anonymous2 <sdgdvs@protonmail.com>
2026-07-19 21:20:51 -0700

    Creator: KDarts Phase 1 - Scaffold and register

---
+++ KDarts/build.bat
@@ -0,0 +1,2 @@
+@echo off
+gcc main.c -o KDarts.exe -mwindows -lgdi32
---
+++ KDarts/main.c
@@ -0,0 +1,51 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) GetStockObject(BLACK_BRUSH));
+            SetTextColor(hdc, RGB(255, 255, 255));
+            SetBkMode(hdc, TRANSPARENT);
+            TextOut(hdc, 10, 10, "KDarts Phase 1 Skeleton", 23);
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const char CLASS_NAME[]  = "KDartsClass";
+    
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    
+    RegisterClass(&wc);
+    
+    HWND hwnd = CreateWindowEx(
+        0, CLASS_NAME, "KDarts", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 600, 600,
+        NULL, NULL, hInstance, NULL
+    );
+    
+    if (hwnd == NULL) {
+        return 0;
+    }
+    
+    ShowWindow(hwnd, nCmdShow);
+    
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    
+    return 0;
+}
