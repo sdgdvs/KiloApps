5b3644df1af2e60126c6050762d7ea984d3354ba
anonymous2 <sdgdvs@protonmail.com>
2026-07-21 20:17:13 -0700

    Creator: KReversi Phase 2 - Implement core web logic

--- KReversi/main.c
+++ KReversi/main.c
@@ -1,22 +1,202 @@
 #include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+#define EMPTY 0
+#define BLACK 1
+#define WHITE 2
 
 const char g_szClassName[] = "KReversiClass";
 
+int board[64];
+int currentPlayer = BLACK;
+int dirs[8][2] = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+void InitGame() {
+    for(int i = 0; i < 64; i++) board[i] = EMPTY;
+    board[27] = WHITE; board[28] = BLACK;
+    board[35] = BLACK; board[36] = WHITE;
+    currentPlayer = BLACK;
+}
+
+int GetFlippable(int index, int player, int* flippable) {
+    if (board[index] != EMPTY) return 0;
+    int r = index / 8;
+    int c = index % 8;
+    int opponent = (player == BLACK) ? WHITE : BLACK;
+    int count = 0;
+
+    for (int d = 0; d < 8; d++) {
+        int nr = r + dirs[d][0];
+        int nc = c + dirs[d][1];
+        int curDirFlippable[8];
+        int curCount = 0;
+
+        while (nr >= 0 && nr < 8 && nc >= 0 && nc < 8 && board[nr * 8 + nc] == opponent) {
+            curDirFlippable[curCount++] = nr * 8 + nc;
+            nr += dirs[d][0];
+            nc += dirs[d][1];
+        }
+        if (nr >= 0 && nr < 8 && nc >= 0 && nc < 8 && board[nr * 8 + nc] == player) {
+            for (int i = 0; i < curCount; i++) {
+                flippable[count++] = curDirFlippable[i];
+            }
+        }
+    }
+    return count;
+}
+
+int HasValidMoves(int player) {
+    int dummy[64];
+    for(int i = 0; i < 64; i++) {
+        if(GetFlippable(i, player, dummy) > 0) return 1;
+    }
+    return 0;
+}
+
+void DoMove(int index, int player) {
+    int flips[64];
+    int count = GetFlippable(index, player, flips);
+    if (count > 0) {
+        board[index] = player;
+        for(int i=0; i<count; i++) {
+            board[flips[i]] = player;
+        }
+    }
+}
+
+void AIMove(HWND hwnd) {
+    if (!HasValidMoves(WHITE)) {
+        currentPlayer = BLACK;
+        InvalidateRect(hwnd, NULL, TRUE);
+        return;
+    }
+    
+    int bestMove = -1;
+    int maxFlips = -1;
+    for(int i = 0; i < 64; i++) {
+        int flips[64];
+        int count = GetFlippable(i, WHITE, flips);
+        if (count > maxFlips) {
+            maxFlips = count;
+            bestMove = i;
+        }
+    }
+    if (bestMove != -1) {
+        DoMove(bestMove, WHITE);
+    }
+    currentPlayer = BLACK;
+    if (!HasValidMoves(BLACK)) {
+        currentPlayer = WHITE;
+    }
+    InvalidateRect(hwnd, NULL, TRUE);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch(msg) {
+        case WM_CREATE:
+            InitGame();
+            break;
+        case WM_LBUTTONDOWN: {
+            if (currentPlayer != BLACK) break;
+            int x = LOWORD(lParam);
+            int y = HIWORD(lParam);
+            
+            int c = x / 50;
+            int r = (y - 40) / 50;
+            
+            if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                int idx = r * 8 + c;
+                int flips[64];
+                int count = GetFlippable(idx, BLACK, flips);
+                if (count > 0) {
+                    DoMove(idx, BLACK);
+                    currentPlayer = WHITE;
+                    if (!HasValidMoves(WHITE)) {
+                        currentPlayer = BLACK;
+                    } else {
+                        InvalidateRect(hwnd, NULL, TRUE);
+                        SetTimer(hwnd, 1, 300, NULL); // small delay for AI
+                    }
+                    InvalidateRect(hwnd, NULL, TRUE);
+                }
+            }
+            break;
+        }
+        case WM_TIMER: {
+            if (wParam == 1) {
+                KillTimer(hwnd, 1);
+                AIMove(hwnd);
+            }
+            break;
+        }
         case WM_PAINT: {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
-            TextOut(hdc, 20, 20, "KReversi - Native Version", 25);
+            
+            int bCount = 0, wCount = 0;
+            for(int i=0; i<64; i++) {
+                if(board[i] == BLACK) bCount++;
+                if(board[i] == WHITE) wCount++;
+            }
+            
+            char scoreStr[64];
+            sprintf(scoreStr, "Black: %d   White: %d", bCount, wCount);
+            TextOut(hdc, 10, 10, scoreStr, strlen(scoreStr));
+            
+            if (!HasValidMoves(BLACK) && !HasValidMoves(WHITE)) {
+                if (bCount > wCount) TextOut(hdc, 200, 10, "Black Wins!", 11);
+                else if (wCount > bCount) TextOut(hdc, 200, 10, "White Wins!", 11);
+                else TextOut(hdc, 200, 10, "Draw!", 5);
+            } else {
+                if (currentPlayer == BLACK) TextOut(hdc, 200, 10, "Turn: Black", 11);
+                else TextOut(hdc, 200, 10, "Turn: White", 11);
+            }
+
+            HBRUSH boardBrush = CreateSolidBrush(RGB(27, 122, 55));
+            HBRUSH blackBrush = CreateSolidBrush(RGB(20, 20, 20));
+            HBRUSH whiteBrush = CreateSolidBrush(RGB(240, 240, 240));
+            HPEN gridPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 0));
+            
+            SelectObject(hdc, gridPen);
+            
+            for(int r = 0; r < 8; r++) {
+                for(int c = 0; c < 8; c++) {
+                    RECT rect = { c * 50, 40 + r * 50, (c + 1) * 50, 40 + (r + 1) * 50 };
+                    SelectObject(hdc, boardBrush);
+                    Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
+                    
+                    int idx = r * 8 + c;
+                    if (board[idx] != EMPTY) {
+                        SelectObject(hdc, board[idx] == BLACK ? blackBrush : whiteBrush);
+                        Ellipse(hdc, rect.left + 5, rect.top + 5, rect.right - 5, rect.bottom - 5);
+                    } else if (currentPlayer == BLACK) {
+                        int dummy[64];
+                        if (GetFlippable(idx, BLACK, dummy) > 0) {
+                            SelectObject(hdc, GetStockObject(NULL_BRUSH));
+                            HPEN hintPen = CreatePen(PS_SOLID, 1, RGB(100, 100, 100));
+                            SelectObject(hdc, hintPen);
+                            Ellipse(hdc, rect.left + 20, rect.top + 20, rect.right - 20, rect.bottom - 20);
+                            DeleteObject(hintPen);
+                        }
+                    }
+                }
+            }
+            
+            DeleteObject(boardBrush);
+            DeleteObject(blackBrush);
+            DeleteObject(whiteBrush);
+            DeleteObject(gridPen);
+            
             EndPaint(hwnd, &ps);
+            break;
         }
-        break;
         case WM_CLOSE:
             DestroyWindow(hwnd);
-        break;
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);
-        break;
+            break;
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
@@ -29,7 +209,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     MSG Msg;
 
     wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.style         = 0;
+    wc.style         = CS_HREDRAW | CS_VREDRAW;
     wc.lpfnWndProc   = WndProc;
     wc.cbClsExtra    = 0;
     wc.cbWndExtra    = 0;
@@ -50,8 +230,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         WS_EX_CLIENTEDGE,
         g_szClassName,
         "KReversi",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 600, 600,
+        WS_OVERLAPPEDWINDOW & ~WS_THICKFRAME & ~WS_MAXIMIZEBOX,
+        CW_USEDEFAULT, CW_USEDEFAULT, 420, 480,
         NULL, NULL, hInstance, NULL);
 
     if(hwnd == NULL) {
--- new_app_plan.md
+++ new_app_plan.md
@@ -51,16 +51,17 @@ When choosing a new app, ask: "Does this do something fundamentally different fr
 ## Current App
 
 **App:** KReversi
-**Phase:** 2
+**Phase:** 3
 **Status:** In Progress
 
 - [x] Phase 1: Choose a unique app. Scaffold `KReversi/` directory. Create web HTML file with basic UI skeleton. Register in App.jsx.
-- [ ] Phase 2: Implement core functionality in the web HTML (inline JS/CSS, self-contained).
+- [x] Phase 2: Implement core functionality in the web HTML (inline JS/CSS, self-contained).
+- [ ] Phase 3: Create native C version (`main.c`, `build.bat`) with Win32 API. Aim for functional parity with web.
 
 ## Next Priority (Before New Apps)
 
 **App:** KReversi
-**Goal:** Phase 2
+**Goal:** Phase 3
 
 
 ## App Lifecycle (14 phases per app)
