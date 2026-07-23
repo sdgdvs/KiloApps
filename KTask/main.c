04afec060b12290f8ec580d823b03442f9f89698
sdgdvs <sdgdvs@users.noreply.github.com>
2026-07-23 00:02:09 -0700

    QA: KTaskMgr - fixed edge cases and accessibility

--- KTask/main.c
+++ KTask/main.c
@@ -7,10 +7,13 @@ void* __cdecl memset(void* p, int c, size_t sz) {
     return p;
 }
 
-HWND hListBox;
-HWND hSearchBox;
-HWND hBtnRefresh, hBtnEndTask;
-HWND hStatusText;
+HWND hListBox = NULL;
+HWND hSearchBox = NULL;
+HWND hBtnRefresh = NULL;
+HWND hBtnEndTask = NULL;
+HWND hStatusText = NULL;
+WNDPROC g_OldEditProc = NULL;
+WNDPROC g_OldListProc = NULL;
 
 void my_utoa(DWORD num, char* str) {
     int i = 0;
@@ -36,14 +39,6 @@ void my_utoa(DWORD num, char* str) {
     }
 }
 
-void my_itoa(int num, char* str) {
-    if (num < 0) {
-        *str++ = '-';
-        num = -num;
-    }
-    my_utoa((DWORD)num, str);
-}
-
 int my_strlen(const char* s) {
     if (!s) return 0;
     int len = 0;
@@ -62,6 +57,18 @@ void my_strcat(char* dest, const char* src) {
     while (*dest) dest++;
     while (*src) *dest++ = *src++;
     *dest = 0;
+}
+
+void my_itoa(int num, char* str) {
+    if (num < 0) {
+        *str++ = '-';
+        if (num == -2147483648) {
+            my_strcpy(str, "2147483648");
+            return;
+        }
+        num = -num;
+    }
+    my_utoa((DWORD)num, str);
 }
 
 int my_stristr(const char* haystack, const char* needle) {
@@ -91,7 +98,8 @@ void LayoutControls(HWND hwnd) {
     if (width < 200 || height < 150) return;
 
     MoveWindow(hSearchBox, 10, 10, width - 20, 25, TRUE);
-    MoveWindow(hListBox, 10, 42, width - 20, height - 78, TRUE);
+    MoveWindow(hListBox, 10, 42, width - 20, height - 100, TRUE);
+    MoveWindow(hStatusText, 10, height - 54, width - 20, 20, TRUE);
     MoveWindow(hBtnRefresh, 10, height - 30, 95, 24, TRUE);
     MoveWindow(hBtnEndTask, width - 105, height - 30, 95, 24, TRUE);
 }
@@ -103,25 +111,32 @@ void RefreshList() {
         selectedPid = (DWORD)SendMessageA(hListBox, LB_GETITEMDATA, currentSel, 0);
     }
 
+    SendMessageA(hListBox, WM_SETREDRAW, FALSE, 0);
     SendMessageA(hListBox, LB_RESETCONTENT, 0, 0);
+
     char filter[256] = {0};
     if (hSearchBox) GetWindowTextA(hSearchBox, filter, sizeof(filter) - 1);
 
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hSnapshot == INVALID_HANDLE_VALUE) {
         SendMessageA(hListBox, LB_ADDSTRING, 0, (LPARAM)"[Error: Unable to snapshot processes]");
+        SendMessageA(hListBox, WM_SETREDRAW, TRUE, 0);
+        InvalidateRect(hListBox, NULL, TRUE);
+        if (hStatusText) SetWindowTextA(hStatusText, "Error: Unable to snapshot processes");
         return;
     }
 
     PROCESSENTRY32 pe32;
     pe32.dwSize = sizeof(PROCESSENTRY32);
     int totalTasks = 0;
+    int shownTasks = 0;
 
     if (Process32First(hSnapshot, &pe32)) {
         do {
+            totalTasks++;
             if (filter[0] && !my_stristr(pe32.szExeFile, filter)) continue;
 
-            char buf[MAX_PATH + 128] = {0};
+            char buf[512] = {0};
             char pidStr[16] = {0};
             char thrStr[16] = {0};
             char priStr[16] = {0};
@@ -142,13 +157,12 @@ void RefreshList() {
 
             int index = SendMessageA(hListBox, LB_ADDSTRING, 0, (LPARAM)buf);
             SendMessageA(hListBox, LB_SETITEMDATA, index, (LPARAM)pe32.th32ProcessID);
-            totalTasks++;
+            shownTasks++;
         } while (Process32Next(hSnapshot, &pe32));
     }
     CloseHandle(hSnapshot);
 
-    // Restore selection if process is still running
-    if (selectedPid != 0 && totalTasks > 0) {
+    if (selectedPid != 0 && shownTasks > 0) {
         int count = SendMessageA(hListBox, LB_GETCOUNT, 0, 0);
         for (int i = 0; i < count; i++) {
             DWORD pid = (DWORD)SendMessageA(hListBox, LB_GETITEMDATA, i, 0);
@@ -157,6 +171,23 @@ void RefreshList() {
                 break;
             }
         }
+    }
+
+    SendMessageA(hListBox, WM_SETREDRAW, TRUE, 0);
+    RedrawWindow(hListBox, NULL, NULL, RDW_ERASE | RDW_FRAME | RDW_INVALIDATE | RDW_ALLCHILDREN);
+
+    if (hStatusText) {
+        char statusBuf[128] = {0};
+        char shownStr[16] = {0};
+        char totalStr[16] = {0};
+        my_utoa((DWORD)shownTasks, shownStr);
+        my_utoa((DWORD)totalTasks, totalStr);
+        my_strcpy(statusBuf, "Processes: ");
+        my_strcat(statusBuf, shownStr);
+        my_strcat(statusBuf, " shown (");
+        my_strcat(statusBuf, totalStr);
+        my_strcat(statusBuf, " total)");
+        SetWindowTextA(hStatusText, statusBuf);
     }
 }
 
@@ -167,8 +198,13 @@ void PerformEndTask(HWND hwnd) {
         return;
     }
 
-    DWORD pid = SendMessageA(hListBox, LB_GETITEMDATA, sel, 0);
+    DWORD pid = (DWORD)SendMessageA(hListBox, LB_GETITEMDATA, sel, 0);
     if (pid == 0) return;
+
+    if (pid == 0 || pid == 4) {
+        MessageBoxA(hwnd, "System and Idle processes cannot be terminated.", "KTask Warning", MB_OK | MB_ICONWARNING);
+        return;
+    }
 
     if (pid == GetCurrentProcessId()) {
         if (MessageBoxA(hwnd, "Terminating KTask will exit this application. Continue?", "Confirm End Task", MB_YESNO | MB_ICONWARNING) != IDYES) {
@@ -182,12 +218,53 @@ void PerformEndTask(HWND hwnd) {
             CloseHandle(hProc);
             RefreshList();
         } else {
+            DWORD err = GetLastError();
             CloseHandle(hProc);
-            MessageBoxA(hwnd, "Failed to terminate process.", "KTask Error", MB_OK | MB_ICONERROR);
+            char errBuf[128] = {0};
+            char errStr[16] = {0};
+            my_utoa(err, errStr);
+            my_strcpy(errBuf, "Failed to terminate process (Error Code: ");
+            my_strcat(errBuf, errStr);
+            my_strcat(errBuf, ").");
+            MessageBoxA(hwnd, errBuf, "KTask Error", MB_OK | MB_ICONERROR);
         }
     } else {
-        MessageBoxA(hwnd, "Access Denied or Process Exited.", "KTask Error", MB_OK | MB_ICONERROR);
+        DWORD err = GetLastError();
+        char errBuf[128] = {0};
+        char errStr[16] = {0};
+        my_utoa(err, errStr);
+        my_strcpy(errBuf, "Access Denied or Process Exited (Error Code: ");
+        my_strcat(errBuf, errStr);
+        my_strcat(errBuf, ").");
+        MessageBoxA(hwnd, errBuf, "KTask Error", MB_OK | MB_ICONERROR);
     }
+}
+
+LRESULT CALLBACK EditSubclassProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_KEYDOWN) {
+        if (wParam == VK_F5) {
+            RefreshList();
+            return 0;
+        } else if (wParam == VK_ESCAPE) {
+            SetWindowTextA(hwnd, "");
+            RefreshList();
+            return 0;
+        }
+    }
+    return CallWindowProcA(g_OldEditProc, hwnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK ListSubclassProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_KEYDOWN) {
+        if (wParam == VK_F5) {
+            RefreshList();
+            return 0;
+        } else if (wParam == VK_DELETE) {
+            PerformEndTask(GetParent(hwnd));
+            return 0;
+        }
+    }
+    return CallWindowProcA(g_OldListProc, hwnd, msg, wParam, lParam);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -195,19 +272,24 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
         case WM_CREATE: {
             hSearchBox = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "", WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, 10, 10, 360, 25, hwnd, (HMENU)3, NULL, NULL);
             hListBox = CreateWindowExA(WS_EX_CLIENTEDGE, "LISTBOX", NULL, WS_CHILD | WS_VISIBLE | WS_VSCROLL | LBS_NOTIFY | LBS_NOINTEGRALHEIGHT, 10, 45, 360, 165, hwnd, (HMENU)4, NULL, NULL);
-            
+            hStatusText = CreateWindowA("STATIC", "Processes: 0", WS_CHILD | WS_VISIBLE | SS_LEFT, 10, 215, 360, 20, hwnd, (HMENU)5, NULL, NULL);
+
             HFONT hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
             SendMessageA(hSearchBox, WM_SETFONT, (WPARAM)hFont, FALSE);
             SendMessageA(hListBox, WM_SETFONT, (WPARAM)hFont, FALSE);
+            SendMessageA(hStatusText, WM_SETFONT, (WPARAM)hFont, FALSE);
 
-            hBtnRefresh = CreateWindowA("BUTTON", "Refresh", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 220, 95, 25, hwnd, (HMENU)1, NULL, NULL);
-            hBtnEndTask = CreateWindowA("BUTTON", "End Task", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 275, 220, 95, 25, hwnd, (HMENU)2, NULL, NULL);
+            hBtnRefresh = CreateWindowA("BUTTON", "Refresh", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 240, 95, 25, hwnd, (HMENU)1, NULL, NULL);
+            hBtnEndTask = CreateWindowA("BUTTON", "End Task", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 275, 240, 95, 25, hwnd, (HMENU)2, NULL, NULL);
             
             SendMessageA(hBtnRefresh, WM_SETFONT, (WPARAM)hFont, FALSE);
             SendMessageA(hBtnEndTask, WM_SETFONT, (WPARAM)hFont, FALSE);
+
+            g_OldEditProc = (WNDPROC)SetWindowLongPtrA(hSearchBox, GWLP_WNDPROC, (LONG_PTR)EditSubclassProc);
+            g_OldListProc = (WNDPROC)SetWindowLongPtrA(hListBox, GWLP_WNDPROC, (LONG_PTR)ListSubclassProc);
             
             RefreshList();
-            SetTimer(hwnd, 1, 2500, NULL); // Auto refresh timer
+            SetTimer(hwnd, 1, 2500, NULL);
             break;
         }
         case WM_SIZE:
@@ -225,11 +307,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             int code = HIWORD(wParam);
             if (id == 3 && code == EN_CHANGE) {
                 RefreshList();
-            } else if (id == 1) { // Refresh button
+            } else if (id == 1) {
                 RefreshList();
-            } else if (id == 2) { // End Task button
+            } else if (id == 2) {
                 PerformEndTask(hwnd);
-            } else if (id == 4 && code == LBN_DBLCLK) { // Double click item in list
+            } else if (id == 4 && code == LBN_DBLCLK) {
                 PerformEndTask(hwnd);
             }
             break;
@@ -259,11 +341,11 @@ void __stdcall MainEntry() {
     wc.hInstance = GetModuleHandleA(NULL);
     wc.lpszClassName = "KTaskClass";
     wc.hCursor = LoadCursorA(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 
     RegisterClassA(&wc);
     
-    RECT rc = {0, 0, 420, 320};
+    RECT rc = {0, 0, 420, 340};
     AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
     
     HWND hwnd = CreateWindowExA(0, "KTaskClass", "KTask Process Monitor", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, wc.hInstance, NULL);
@@ -280,4 +362,3 @@ void __stdcall MainEntry() {
     }
     ExitProcess(0);
 }
-
