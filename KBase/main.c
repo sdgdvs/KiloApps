95d45c01c65b0562cca9a93683441322cbb49ce0
sdgdvs <sdgdvs@users.noreply.github.com>
2026-07-24 12:01:11 -0700

    QA: KCalendar - fixed regressions, date math bounds, and accessibility

--- KBase/main.c
+++ KBase/main.c
@@ -5,6 +5,7 @@
 #pragma comment (lib, "crypt32.lib")
 #pragma comment (lib, "advapi32.lib")
 #pragma comment (lib, "user32.lib")
+#pragma comment (lib, "gdi32.lib")
 
 #pragma function(memset)
 void* __cdecl memset(void* dest, int c, size_t count) {
@@ -13,9 +14,92 @@ void* __cdecl memset(void* dest, int c, size_t count) {
     return dest;
 }
 
-HWND hInput, hOutput;
-HWND hBtnEnc, hBtnDec, hBtnHash;
+#pragma function(memcpy)
+void* __cdecl memcpy(void* dest, const void* src, size_t count) {
+    char* d = (char*)dest;
+    const char* s = (const char*)src;
+    while (count--) *d++ = *s++;
+    return dest;
+}
 
+HWND hInput, hOutput, hBitDisplay, hEditA, hEditB, hBtnEnc, hBtnDec, hBtnHash;
+
+// Custom String Helpers (No CRT dependencies)
+static size_t my_strlen(const char* s) {
+    size_t len = 0;
+    while (s && s[len]) len++;
+    return len;
+}
+
+static int parse_hex_digit(char c) {
+    if (c >= '0' && c <= '9') return c - '0';
+    if (c >= 'a' && c <= 'f') return c - 'a' + 10;
+    if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+    return -1;
+}
+
+static UINT64 parse_u64(const char* str, int base) {
+    UINT64 res = 0;
+    if (!str) return 0;
+    
+    // Skip 0x / 0b / 0o prefix if present
+    if (base == 16 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')) str += 2;
+    if (base == 2 && str[0] == '0' && (str[1] == 'b' || str[1] == 'B')) str += 2;
+
+    while (*str) {
+        int digit = -1;
+        char c = *str;
+        if (base == 16) {
+            digit = parse_hex_digit(c);
+        } else if (base == 10) {
+            if (c >= '0' && c <= '9') digit = c - '0';
+        } else if (base == 2) {
+            if (c == '0' || c == '1') digit = c - '0';
+        } else if (base == 8) {
+            if (c >= '0' && c <= '7') digit = c - '0';
+        }
+        if (digit < 0 || digit >= base) break;
+        res = res * base + digit;
+        str++;
+    }
+    return res;
+}
+
+static void u64_to_hex(UINT64 val, char* outBuf) {
+    wsprintfA(outBuf, "%08X%08X", (DWORD)(val >> 32), (DWORD)(val & 0xFFFFFFFF));
+}
+
+static void u64_to_bin(UINT64 val, char* outBuf) {
+    for (int i = 63; i >= 0; i--) {
+        outBuf[63 - i] = ((val >> i) & 1) ? '1' : '0';
+    }
+    outBuf[64] = '\0';
+}
+
+static void u64_to_dec(UINT64 val, char* outBuf) {
+    if (val == 0) {
+        outBuf[0] = '0';
+        outBuf[1] = '\0';
+        return;
+    }
+    char temp[32];
+    int pos = 0;
+    while (val > 0) {
+        temp[pos++] = '0' + (char)(val % 10);
+        val /= 10;
+    }
+    int outPos = 0;
+    for (int i = pos - 1; i >= 0; i--) {
+        outBuf[outPos++] = temp[i];
+    }
+    outBuf[outPos] = '\0';
+}
+
+// Window Controls
+HWND hInput, hOutput, hBitDisplay;
+HWND hEditA, hEditB;
+
+// Core Utility Operations
 void DoB64Encode() {
     DWORD len = GetWindowTextLengthA(hInput);
     if (len == 0) return;
@@ -42,11 +126,11 @@ void DoB64Decode() {
             outBuf[outLen] = 0;
             SetWindowTextA(hOutput, outBuf);
         } else {
-            SetWindowTextA(hOutput, "Error decoding.");
+            SetWindowTextA(hOutput, "Error decoding Base64.");
         }
         HeapFree(GetProcessHeap(), 0, outBuf);
     } else {
-        SetWindowTextA(hOutput, "Error decoding.");
+        SetWindowTextA(hOutput, "Error decoding Base64.");
     }
     HeapFree(GetProcessHeap(), 0, buf);
 }
@@ -111,7 +195,7 @@ void DoUrlDecode() {
     for (DWORD i = 0; i < len; i++) {
         if (buf[i] == '%' && i + 2 < len) {
             char hex[3] = { buf[i+1], buf[i+2], 0 };
-            *p++ = (char)strtol(hex, NULL, 16);
+            *p++ = (char)parse_hex_digit(hex[0]) * 16 + parse_hex_digit(hex[1]);
             i += 2;
         } else if (buf[i] == '+') {
             *p++ = ' ';
@@ -153,9 +237,12 @@ void DoHexDecode() {
         char c = buf[i];
         if (c == ' ' || c == '\t' || c == '\r' || c == '\n') continue;
         if (i + 1 < len) {
-            char hex[3] = { buf[i], buf[i+1], 0 };
-            outBuf[outLen++] = (char)strtol(hex, NULL, 16);
-            i++;
+            int d1 = parse_hex_digit(buf[i]);
+            int d2 = parse_hex_digit(buf[i+1]);
+            if (d1 >= 0 && d2 >= 0) {
+                outBuf[outLen++] = (char)(d1 * 16 + d2);
+                i++;
+            }
         }
     }
     outBuf[outLen] = 0;
@@ -164,39 +251,141 @@ void DoHexDecode() {
     HeapFree(GetProcessHeap(), 0, outBuf);
 }
 
+// Convert Base live
+void DoConvertBases() {
+    char buf[128];
+    GetWindowTextA(hInput, buf, 128);
+    UINT64 val = parse_u64(buf, 10);
+    if (val == 0 && buf[0] != '0') {
+        val = parse_u64(buf, 16);
+    }
+    
+    char hex[32], dec[32], bin[68], formatted[256];
+    u64_to_hex(val, hex);
+    u64_to_dec(val, dec);
+    u64_to_bin(val, bin);
+
+    wsprintfA(formatted, "DEC: %s\r\nHEX: 0x%s\r\nBIN: %s", dec, hex, bin);
+    SetWindowTextA(hOutput, formatted);
+    SetWindowTextA(hBitDisplay, bin);
+}
+
+// Bitwise operations (AND, OR, XOR, NOT, SHL, SHR, ROL, ROR)
+void DoBitwiseOp(int op) {
+    char bufA[64], bufB[64];
+    GetWindowTextA(hEditA, bufA, 64);
+    GetWindowTextA(hEditB, bufB, 64);
+
+    UINT64 valA = parse_u64(bufA, 16);
+    if (valA == 0 && bufA[0] != '0') valA = parse_u64(bufA, 10);
+
+    UINT64 valB = parse_u64(bufB, 16);
+    if (valB == 0 && bufB[0] != '0') valB = parse_u64(bufB, 10);
+
+    UINT64 res = 0;
+    const char* opName = "";
+
+    switch (op) {
+        case 10: res = valA & valB; opName = "AND"; break;
+        case 11: res = valA | valB; opName = "OR"; break;
+        case 12: res = valA ^ valB; opName = "XOR"; break;
+        case 13: res = ~valA; opName = "NOT(A)"; break;
+        case 14: res = valA << (valB & 63); opName = "SHL"; break;
+        case 15: res = valA >> (valB & 63); opName = "SHR"; break;
+        case 16: {
+            UINT64 cnt = valB & 63;
+            res = (valA << cnt) | (valA >> (64 - cnt));
+            opName = "ROL";
+            break;
+        }
+        case 17: {
+            UINT64 cnt = valB & 63;
+            res = (valA >> cnt) | (valA << (64 - cnt));
+            opName = "ROR";
+            break;
+        }
+    }
+
+    char hex[32], dec[32], bin[68], outStr[256];
+    u64_to_hex(res, hex);
+    u64_to_dec(res, dec);
+    u64_to_bin(res, bin);
+
+    wsprintfA(outStr, "[Bitwise %s Result]\r\nHEX: 0x%s\r\nDEC: %s\r\nBIN: %s", opName, hex, dec, bin);
+    SetWindowTextA(hOutput, outStr);
+    SetWindowTextA(hBitDisplay, bin);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_CREATE: {
             HFONT hFont = CreateFontA(14, 0, 0, 0, FW_NORMAL, 0, 0, 0, DEFAULT_CHARSET, 0, 0, DEFAULT_QUALITY, DEFAULT_PITCH, "Consolas");
             
-            hInput = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_WANTRETURN,
-                10, 10, 360, 100, hwnd, NULL, NULL, NULL);
+            CreateWindowA("STATIC", "Input Buffer / Number:", WS_CHILD | WS_VISIBLE, 10, 10, 200, 18, hwnd, NULL, NULL, NULL);
+            hInput = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "42", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_WANTRETURN,
+                10, 30, 480, 50, hwnd, NULL, NULL, NULL);
             SendMessageA(hInput, WM_SETFONT, (WPARAM)hFont, 0);
-            
-            hBtnEnc = CreateWindowA("BUTTON", "B64 Enc", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 120, 80, 25, hwnd, (HMENU)1, NULL, NULL);
+
+            // Operands A & B for bitwise calculations
+            CreateWindowA("STATIC", "Operand A (Hex/Dec):", WS_CHILD | WS_VISIBLE, 10, 88, 140, 18, hwnd, NULL, NULL, NULL);
+            hEditA = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "0x0F0F0F0F", WS_CHILD | WS_VISIBLE, 150, 85, 120, 22, hwnd, NULL, NULL, NULL);
+            SendMessageA(hEditA, WM_SETFONT, (WPARAM)hFont, 0);
+
+            CreateWindowA("STATIC", "Operand B / Shift:", WS_CHILD | WS_VISIBLE, 280, 88, 120, 18, hwnd, NULL, NULL, NULL);
+            hEditB = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "0x00FF00FF", WS_CHILD | WS_VISIBLE, 400, 85, 90, 22, hwnd, NULL, NULL, NULL);
+            SendMessageA(hEditB, WM_SETFONT, (WPARAM)hFont, 0);
+
+            // Action Buttons - Base & Text Encoding
+            HWND hBtnConv = CreateWindowA("BUTTON", "Convert Base", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 115, 95, 24, hwnd, (HMENU)100, NULL, NULL);
+            SendMessageA(hBtnConv, WM_SETFONT, (WPARAM)hFont, 0);
+
+            hBtnEnc = CreateWindowA("BUTTON", "B64 Enc", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 110, 115, 70, 24, hwnd, (HMENU)1, NULL, NULL);
             SendMessageA(hBtnEnc, WM_SETFONT, (WPARAM)hFont, 0);
             
-            hBtnDec = CreateWindowA("BUTTON", "B64 Dec", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 100, 120, 80, 25, hwnd, (HMENU)2, NULL, NULL);
+            hBtnDec = CreateWindowA("BUTTON", "B64 Dec", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 185, 115, 70, 24, hwnd, (HMENU)2, NULL, NULL);
             SendMessageA(hBtnDec, WM_SETFONT, (WPARAM)hFont, 0);
             
-            HWND hBtnUrlEnc = CreateWindowA("BUTTON", "URL Enc", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 190, 120, 80, 25, hwnd, (HMENU)4, NULL, NULL);
+            HWND hBtnUrlEnc = CreateWindowA("BUTTON", "URL Enc", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 260, 115, 70, 24, hwnd, (HMENU)4, NULL, NULL);
             SendMessageA(hBtnUrlEnc, WM_SETFONT, (WPARAM)hFont, 0);
 
-            HWND hBtnUrlDec = CreateWindowA("BUTTON", "URL Dec", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 280, 120, 80, 25, hwnd, (HMENU)5, NULL, NULL);
+            HWND hBtnUrlDec = CreateWindowA("BUTTON", "URL Dec", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 335, 115, 70, 24, hwnd, (HMENU)5, NULL, NULL);
             SendMessageA(hBtnUrlDec, WM_SETFONT, (WPARAM)hFont, 0);
 
-            HWND hBtnHexEnc = CreateWindowA("BUTTON", "Hex Enc", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 155, 80, 25, hwnd, (HMENU)6, NULL, NULL);
-            SendMessageA(hBtnHexEnc, WM_SETFONT, (WPARAM)hFont, 0);
-
-            HWND hBtnHexDec = CreateWindowA("BUTTON", "Hex Dec", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 100, 155, 80, 25, hwnd, (HMENU)7, NULL, NULL);
-            SendMessageA(hBtnHexDec, WM_SETFONT, (WPARAM)hFont, 0);
-            
-            hBtnHash = CreateWindowA("BUTTON", "SHA-256", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 190, 155, 80, 25, hwnd, (HMENU)3, NULL, NULL);
+            hBtnHash = CreateWindowA("BUTTON", "SHA-256", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 410, 115, 80, 24, hwnd, (HMENU)3, NULL, NULL);
             SendMessageA(hBtnHash, WM_SETFONT, (WPARAM)hFont, 0);
-            
+
+            // Bitwise Operator Buttons
+            HWND hBtnAnd = CreateWindowA("BUTTON", "AND", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 145, 55, 24, hwnd, (HMENU)10, NULL, NULL);
+            SendMessageA(hBtnAnd, WM_SETFONT, (WPARAM)hFont, 0);
+            HWND hBtnOr  = CreateWindowA("BUTTON", "OR", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 70, 145, 55, 24, hwnd, (HMENU)11, NULL, NULL);
+            SendMessageA(hBtnOr, WM_SETFONT, (WPARAM)hFont, 0);
+            HWND hBtnXor = CreateWindowA("BUTTON", "XOR", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 130, 145, 55, 24, hwnd, (HMENU)12, NULL, NULL);
+            SendMessageA(hBtnXor, WM_SETFONT, (WPARAM)hFont, 0);
+            HWND hBtnNot = CreateWindowA("BUTTON", "NOT(A)", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 190, 145, 60, 24, hwnd, (HMENU)13, NULL, NULL);
+            SendMessageA(hBtnNot, WM_SETFONT, (WPARAM)hFont, 0);
+            HWND hBtnShl = CreateWindowA("BUTTON", "SHL", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 255, 145, 55, 24, hwnd, (HMENU)14, NULL, NULL);
+            SendMessageA(hBtnShl, WM_SETFONT, (WPARAM)hFont, 0);
+            HWND hBtnShr = CreateWindowA("BUTTON", "SHR", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 315, 145, 55, 24, hwnd, (HMENU)15, NULL, NULL);
+            SendMessageA(hBtnShr, WM_SETFONT, (WPARAM)hFont, 0);
+            HWND hBtnRol = CreateWindowA("BUTTON", "ROL", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 375, 145, 55, 24, hwnd, (HMENU)16, NULL, NULL);
+            SendMessageA(hBtnRol, WM_SETFONT, (WPARAM)hFont, 0);
+            HWND hBtnRor = CreateWindowA("BUTTON", "ROR", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 435, 145, 55, 24, hwnd, (HMENU)17, NULL, NULL);
+            SendMessageA(hBtnRor, WM_SETFONT, (WPARAM)hFont, 0);
+
+            // 64-Bit Binary Stream Display
+            CreateWindowA("STATIC", "64-Bit Binary Stream:", WS_CHILD | WS_VISIBLE, 10, 178, 200, 18, hwnd, NULL, NULL, NULL);
+            hBitDisplay = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "0000000000000000000000000000000000000000000000000000000000000000",
+                WS_CHILD | WS_VISIBLE | ES_READONLY, 10, 196, 480, 24, hwnd, NULL, NULL, NULL);
+            SendMessageA(hBitDisplay, WM_SETFONT, (WPARAM)hFont, 0);
+
+            // Output Display Area
+            CreateWindowA("STATIC", "Output Result:", WS_CHILD | WS_VISIBLE, 10, 226, 200, 18, hwnd, NULL, NULL, NULL);
             hOutput = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_READONLY,
-                10, 190, 360, 100, hwnd, NULL, NULL, NULL);
+                10, 245, 480, 110, hwnd, NULL, NULL, NULL);
             SendMessageA(hOutput, WM_SETFONT, (WPARAM)hFont, 0);
+
+            // Trigger default conversion
+            DoConvertBases();
             break;
         }
         case WM_COMMAND: {
@@ -208,6 +397,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             else if (id == 5) DoUrlDecode();
             else if (id == 6) DoHexEncode();
             else if (id == 7) DoHexDecode();
+            else if (id == 100) DoConvertBases();
+            else if (id >= 10 && id <= 17) DoBitwiseOp(id);
             break;
         }
         case WM_CTLCOLOREDIT:
@@ -236,10 +427,10 @@ void __stdcall MainEntry() {
     
     RegisterClassA(&wc);
     
-    RECT rc = {0, 0, 380, 300};
+    RECT rc = {0, 0, 500, 370};
     AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX, FALSE);
     
-    HWND hwnd = CreateWindowExA(0, "KBaseApp", "KBase", WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
+    HWND hwnd = CreateWindowExA(0, "KBaseApp", "KBase - Universal Base & Bitwise Utility", WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
         CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, wc.hInstance, NULL);
         
     ShowWindow(hwnd, SW_SHOW);
--- KCalendar/main.c
+++ KCalendar/main.c
@@ -1,48 +1,121 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <commctrl.h>
-#include <stdio.h>
-#include <string.h>
 
-#define ID_MONTHCAL 1000
-#define ID_BTN_TODAY 1001
+#define ID_MONTHCAL    1000
+#define ID_BTN_TODAY   1001
 #define ID_LIST_EVENTS 1002
-#define ID_EDIT_EVENT 1003
-#define ID_BTN_ADD 1004
+#define ID_EDIT_EVENT  1003
+#define ID_BTN_ADD     1004
+#define ID_BTN_DEL     1005
 
 #define MAX_EVENTS 1000
+
 typedef struct {
     int year, month, day;
     char text[128];
 } Event;
 
-Event events[MAX_EVENTS];
-int event_count = 0;
-SYSTEMTIME selected_date;
+static Event events[MAX_EVENTS];
+static int event_count = 0;
+static SYSTEMTIME selected_date;
 
-HWND hMonthCal, hBtnToday, hListEvents, hEditEvent, hBtnAdd;
+static HWND hMonthCal, hBtnToday, hListEvents, hEditEvent, hBtnAdd, hBtnDel;
+static HBRUSH hBgBrush = NULL;
+static HBRUSH hEditBrush = NULL;
+static WNDPROC oldEditProc = NULL;
 
-void LoadEvents() {
-    FILE *f = fopen("kcal_events.dat", "r");
-    if (!f) return;
-    event_count = 0;
-    while (fscanf(f, "%d %d %d %[^\n]\n", &events[event_count].year, &events[event_count].month, &events[event_count].day, events[event_count].text) == 4) {
-        event_count++;
-        if (event_count >= MAX_EVENTS) break;
-    }
-    fclose(f);
+// --- CRT-free Helper Functions ---
+void* __cdecl memset(void* p, int c, size_t sz) {
+    char* pb = (char*)p;
+    while (sz--) *pb++ = (char)c;
+    return p;
+}
+#pragma function(memset)
+
+static int my_strlen(const char* s) {
+    if (!s) return 0;
+    int len = 0;
+    while (s[len]) len++;
+    return len;
 }
 
-void SaveEvents() {
-    FILE *f = fopen("kcal_events.dat", "w");
-    if (!f) return;
+static void my_strcpy(char* dest, const char* src) {
+    if (!dest || !src) return;
+    while (*src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+}
+
+static void LoadEvents() {
+    HANDLE hFile = CreateFileA("kcal_events.dat", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) return;
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    if (fileSize == 0 || fileSize == INVALID_FILE_SIZE) {
+        CloseHandle(hFile);
+        return;
+    }
+
+    char* buf = (char*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, fileSize + 1);
+    if (!buf) {
+        CloseHandle(hFile);
+        return;
+    }
+
+    DWORD bytesRead = 0;
+    if (ReadFile(hFile, buf, fileSize, &bytesRead, NULL)) {
+        buf[bytesRead] = '\0';
+        event_count = 0;
+        char* ptr = buf;
+        while (*ptr && event_count < MAX_EVENTS) {
+            int y = 0, m = 0, d = 0;
+            while (*ptr == ' ' || *ptr == '\t' || *ptr == '\r' || *ptr == '\n') ptr++;
+            if (!*ptr) break;
+
+            while (*ptr >= '0' && *ptr <= '9') { y = y * 10 + (*ptr - '0'); ptr++; }
+            while (*ptr == ' ') ptr++;
+            while (*ptr >= '0' && *ptr <= '9') { m = m * 10 + (*ptr - '0'); ptr++; }
+            while (*ptr == ' ') ptr++;
+            while (*ptr >= '0' && *ptr <= '9') { d = d * 10 + (*ptr - '0'); ptr++; }
+            while (*ptr == ' ') ptr++;
+
+            int textIdx = 0;
+            while (*ptr && *ptr != '\r' && *ptr != '\n' && textIdx < 127) {
+                events[event_count].text[textIdx++] = *ptr++;
+            }
+            events[event_count].text[textIdx] = '\0';
+
+            while (*ptr && *ptr != '\r' && *ptr != '\n') ptr++;
+
+            if (y > 0 && m >= 1 && m <= 12 && d >= 1 && d <= 31 && textIdx > 0) {
+                events[event_count].year = y;
+                events[event_count].month = m;
+                events[event_count].day = d;
+                event_count++;
+            }
+        }
+    }
+
+    HeapFree(GetProcessHeap(), 0, buf);
+    CloseHandle(hFile);
+}
+
+static void SaveEvents() {
+    HANDLE hFile = CreateFileA("kcal_events.dat", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) return;
+
     for (int i = 0; i < event_count; i++) {
-        fprintf(f, "%d %d %d %s\n", events[i].year, events[i].month, events[i].day, events[i].text);
+        char line[256];
+        int len = wsprintfA(line, "%d %d %d %s\r\n", events[i].year, events[i].month, events[i].day, events[i].text);
+        DWORD written = 0;
+        WriteFile(hFile, line, len, &written, NULL);
     }
-    fclose(f);
+    CloseHandle(hFile);
 }
 
-void RefreshList() {
+static void RefreshList() {
     SendMessage(hListEvents, LB_RESETCONTENT, 0, 0);
     for (int i = 0; i < event_count; i++) {
         if (events[i].year == selected_date.wYear && 
@@ -53,6 +126,59 @@ void RefreshList() {
     }
 }
 
+static int GetEventIndexFromListIndex(int selIndex) {
+    if (selIndex < 0) return -1;
+    int current = 0;
+    for (int i = 0; i < event_count; i++) {
+        if (events[i].year == selected_date.wYear && 
+            events[i].month == selected_date.wMonth && 
+            events[i].day == selected_date.wDay) {
+            if (current == selIndex) return i;
+            current++;
+        }
+    }
+    return -1;
+}
+
+static void DeleteSelectedEvent() {
+    int sel = (int)SendMessage(hListEvents, LB_GETCURSEL, 0, 0);
+    if (sel == LB_ERR) return;
+    int idx = GetEventIndexFromListIndex(sel);
+    if (idx >= 0 && idx < event_count) {
+        for (int i = idx; i < event_count - 1; i++) {
+            events[i] = events[i + 1];
+        }
+        event_count--;
+        SaveEvents();
+        RefreshList();
+        InvalidateRect(hMonthCal, NULL, TRUE);
+    }
+}
+
+static void AddEventFromInput() {
+    char buf[128];
+    GetWindowText(hEditEvent, buf, 128);
+    if (my_strlen(buf) > 0 && event_count < MAX_EVENTS) {
+        events[event_count].year = selected_date.wYear;
+        events[event_count].month = selected_date.wMonth;
+        events[event_count].day = selected_date.wDay;
+        my_strcpy(events[event_count].text, buf);
+        event_count++;
+        SaveEvents();
+        SetWindowText(hEditEvent, "");
+        RefreshList();
+        InvalidateRect(hMonthCal, NULL, TRUE);
+    }
+}
+
+static LRESULT CALLBACK EditSubclassProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_KEYDOWN && wParam == VK_RETURN) {
+        AddEventFromInput();
+        return 0;
+    }
+    return CallWindowProc(oldEditProc, hwnd, msg, wParam, lParam);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_CREATE: {
@@ -60,6 +186,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             icex.dwSize = sizeof(icex);
             icex.dwICC = ICC_DATE_CLASSES;
             InitCommonControlsEx(&icex);
+
+            hBgBrush = CreateSolidBrush(RGB(15, 23, 42));
+            hEditBrush = CreateSolidBrush(RGB(15, 23, 42));
 
             LoadEvents();
             GetLocalTime(&selected_date);
@@ -77,18 +206,27 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 10, 10 + rc.bottom + 10, rc.right, 30, hwnd, (HMENU)ID_BTN_TODAY, GetModuleHandle(NULL), NULL);
 
             int listX = 10 + rc.right + 10;
-            int listW = 200;
+            int listW = 220;
+            int listH = rc.bottom - 40;
+
             hListEvents = CreateWindowEx(WS_EX_CLIENTEDGE, "LISTBOX", "",
                 WS_CHILD | WS_VISIBLE | WS_VSCROLL | LBS_NOTIFY,
-                listX, 10, listW, rc.bottom - 40, hwnd, (HMENU)ID_LIST_EVENTS, GetModuleHandle(NULL), NULL);
+                listX, 10, listW, listH, hwnd, (HMENU)ID_LIST_EVENTS, GetModuleHandle(NULL), NULL);
 
             hEditEvent = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "",
                 WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
-                listX, 10 + rc.bottom - 30, listW, 25, hwnd, (HMENU)ID_EDIT_EVENT, GetModuleHandle(NULL), NULL);
+                listX, 10 + listH + 5, listW, 25, hwnd, (HMENU)ID_EDIT_EVENT, GetModuleHandle(NULL), NULL);
 
-            hBtnAdd = CreateWindowEx(0, "BUTTON", "Add Event",
+            int btnW = (listW - 5) / 2;
+            hBtnAdd = CreateWindowEx(0, "BUTTON", "Add",
                 WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                listX, 10 + rc.bottom + 10, listW, 30, hwnd, (HMENU)ID_BTN_ADD, GetModuleHandle(NULL), NULL);
+                listX, 10 + rc.bottom + 10, btnW, 30, hwnd, (HMENU)ID_BTN_ADD, GetModuleHandle(NULL), NULL);
+
+            hBtnDel = CreateWindowEx(0, "BUTTON", "Delete",
+                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                listX + btnW + 5, 10 + rc.bottom + 10, btnW, 30, hwnd, (HMENU)ID_BTN_DEL, GetModuleHandle(NULL), NULL);
+
+            oldEditProc = (WNDPROC)SetWindowLongPtr(hEditEvent, GWLP_WNDPROC, (LONG_PTR)EditSubclassProc);
 
             RECT winRc = {0, 0, listX + listW + 10, 10 + rc.bottom + 10 + 30 + 10};
             AdjustWindowRect(&winRc, GetWindowLong(hwnd, GWL_STYLE), FALSE);
@@ -119,7 +257,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                         MONTHDAYSTATE state = 0;
                         for (int e = 0; e < event_count; e++) {
                             if (events[e].year == st.wYear && events[e].month == st.wMonth) {
-                                state |= (1 << (events[e].day - 1));
+                                if (events[e].day >= 1 && events[e].day <= 31) {
+                                    state |= (1 << (events[e].day - 1));
+                                }
                             }
                         }
                         lpNMDayState->prgDayState[i] = state;
@@ -136,19 +276,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 SendMessage(hMonthCal, MCM_SETCURSEL, 0, (LPARAM)&selected_date);
                 RefreshList();
             } else if (LOWORD(wParam) == ID_BTN_ADD) {
-                char buf[128];
-                GetWindowText(hEditEvent, buf, 128);
-                if (strlen(buf) > 0 && event_count < MAX_EVENTS) {
-                    events[event_count].year = selected_date.wYear;
-                    events[event_count].month = selected_date.wMonth;
-                    events[event_count].day = selected_date.wDay;
-                    strcpy(events[event_count].text, buf);
-                    event_count++;
-                    SaveEvents();
-                    SetWindowText(hEditEvent, "");
-                    RefreshList();
-                    InvalidateRect(hMonthCal, NULL, TRUE);
-                }
+                AddEventFromInput();
+            } else if (LOWORD(wParam) == ID_BTN_DEL) {
+                DeleteSelectedEvent();
+            } else if (LOWORD(wParam) == ID_LIST_EVENTS && HIWORD(wParam) == LBN_DBLCLK) {
+                DeleteSelectedEvent();
             }
             break;
         case WM_CTLCOLORLISTBOX:
@@ -156,11 +288,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             HDC hdc = (HDC)wParam;
             SetBkColor(hdc, RGB(15, 23, 42));
             SetTextColor(hdc, RGB(255, 255, 255));
-            static HBRUSH hBrush = NULL;
-            if (!hBrush) hBrush = CreateSolidBrush(RGB(15, 23, 42));
-            return (LRESULT)hBrush;
+            return (LRESULT)hEditBrush;
         }
         case WM_DESTROY:
+            if (hBgBrush) { DeleteObject(hBgBrush); hBgBrush = NULL; }
+            if (hEditBrush) { DeleteObject(hEditBrush); hEditBrush = NULL; }
             PostQuitMessage(0);
             break;
         default:
@@ -180,7 +312,7 @@ void MainEntry() {
     RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(0, "KCalendarApp", "KCalendar", WS_OVERLAPPEDWINDOW & ~WS_THICKFRAME & ~WS_MAXIMIZEBOX,
-        CW_USEDEFAULT, CW_USEDEFAULT, 400, 300, NULL, NULL, hInstance, NULL);
+        CW_USEDEFAULT, CW_USEDEFAULT, 460, 320, NULL, NULL, hInstance, NULL);
 
     ShowWindow(hwnd, SW_SHOW);
     UpdateWindow(hwnd);
@@ -192,3 +324,4 @@ void MainEntry() {
     }
     ExitProcess(0);
 }
+
