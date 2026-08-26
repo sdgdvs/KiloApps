8910cf73f98e8c502b46871810f3746450e6449f
anonymous2 <sdgdvs@protonmail.com>
2026-08-26 16:17:05 -0700

    Phase 1: Scaffold KColosseum directory, create web HTML skeleton, register in App.jsx

---
+++ KColosseum/build.bat
@@ -0,0 +1,2 @@
+@echo off
+gcc main.c -o KColosseum.exe -mwindows
---
+++ KColosseum/main.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MessageBox(NULL, "KColosseum Phase 1 Scaffold", "KColosseum", MB_OK);
+    return 0;
+}
